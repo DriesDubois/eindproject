d5c61311441afac4514fd513a98741121e0d852a
Dries Dubois <dries.dubois@gmail.com>
2022-11-02 15:43:57 +0100

    - fixing security stuff with api key

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -3,7 +3,7 @@ import {getFirestore} from 'firebase/firestore';
 import {getAuth} from 'firebase/auth'
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_APIKEY,
+    // apiKey: process.env.REACT_APP_APIKEY,
     authDomain: process.env.REACT_APP_AUTHDOMAIN,
     projectId: process.env.REACT_APP_FIREBASE_ID,
     storageBucket: process.env.REACT_APP_STORAGEBUCKET,
