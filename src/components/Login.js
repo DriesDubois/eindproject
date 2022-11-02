70b5601bcb1c6312283c929e803d2dbd1ab2994c
Dries Dubois <dries.dubois@gmail.com>
2022-11-02 13:31:08 +0100

    + logout functionality
     TODO: make it so you can't access profile page without being logged in

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,6 +2,10 @@ import {useState} from 'react'
 import { Link } from 'react-router-dom'
 import { useAuthValue } from "../contexts/AuthContext"
 
+import {signInWithEmailAndPassword, sendEmailVerification} from 'firebase/auth'
+import {auth} from '../services/firebase'
+import {useNavigate} from 'react-router-dom'
+
 function Login(){
 
     const [email, setEmail] = useState('')
@@ -9,13 +13,33 @@ function Login(){
     const [error, setError] = useState('')
     const {currentUser} = useAuthValue()
     console.log(currentUser)
+    const {setTimeActive} = useAuthValue()
+    const history = useNavigate()
+
+    const login = e => {
+        e.preventDefault()
+        signInWithEmailAndPassword(auth, email, password)
+            .then(() => {
+                if(!auth.currentUser.emailVerified) {
+                    sendEmailVerification(auth.currentUser)
+                        .then(() => {
+                            setTimeActive(true)
+                            history('/VerifyEmail')
+                        })
+                        .catch(err => alert(err.message))
+                }else{
+                    history('/Profile')
+                }
+            })
+            .catch(err => setError(err.message))
+    }
 
     return(
         <div className='center'>
             <div className='auth'>
                 <h1>Log in</h1>
                 {error && <div className='auth__error'>{error}</div>}
-                <form name='login_form'>
+                <form onSubmit={login} name='login_form'>
                     <input
                         type='email'
                         value={email}
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,12 +1,20 @@
+import {useAuthValue} from '../contexts/AuthContext'
+import { signOut } from 'firebase/auth'
+import { auth } from '../services/firebase'
+
 export function Profile() {
+    const {currentUser} = useAuthValue()
 
     return (
         <div className='center'>
             <div className='profile'>
                 <h1>Profile</h1>
-                <p><strong>Email: </strong> </p>
-                <p><strong>Email verified: </strong> </p>
-                <span>Sign Out</span>
+                <p><strong>Email: </strong>{currentUser?.email}</p>
+                <p>
+                    <strong>Email verified: </strong>
+                    {`${currentUser?.emailVerified}`}
+                </p>
+                <span onClick={() => signOut(auth)}>Sign Out</span>
             </div>
         </div>
     )
