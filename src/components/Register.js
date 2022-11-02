28fc092c7dbc01fffa9ca70fc090b9c72a84f948
Dries Dubois <dries.dubois@gmail.com>
2022-11-02 12:23:47 +0100

    + start of registration functionality using firebase

---
+++ src/components/Register.js
@@ -0,0 +1,79 @@
+import {useState} from 'react'
+import { Link } from 'react-router-dom'
+import './forms.css'
+import {auth} from './firebase'
+import {createUserWithEmailAndPassword} from 'firebase/auth'
+
+function Register() {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+    const [error, setError] = useState('')
+
+    const validatePassword = () => {
+        let isValid = true
+        if (password !== '' && confirmPassword !== ''){
+            if (password !== confirmPassword) {
+                isValid = false
+                setError('Passwords does not match')
+            }
+        }
+        return isValid
+    }
+
+    const register = e => {
+        e.preventDefault()
+        setError('')
+        if(validatePassword()) {
+            // Create a new user with email and password using firebase
+            createUserWithEmailAndPassword(auth, email, password)
+                .then((res) => {
+                    console.log(res.user)
+                })
+                .catch(err => setError(err.message))
+        }
+        setEmail('')
+        setPassword('')
+        setConfirmPassword('')
+    }
+
+    return (
+        <div className='center'>
+            <div className='auth'>
+                <h1>Register</h1>
+                {error && <div className='auth__error'>{error}</div>}
+                <form onSubmit={register} name='registration_form'>
+                    <input
+                        type='email'
+                        value={email}
+                        placeholder="Enter your email"
+                        required
+                        onChange={e => setEmail(e.target.value)}/>
+
+                    <input
+                        type='password'
+                        value={password}
+                        required
+                        placeholder='Enter your password'
+                        onChange={e => setPassword(e.target.value)}/>
+
+                    <input
+                        type='password'
+                        value={confirmPassword}
+                        required
+                        placeholder='Confirm password'
+                        onChange={e => setConfirmPassword(e.target.value)}/>
+
+                    <button type='submit'>Register</button>
+                </form>
+                <span>
+          Already have an account?
+          <Link to='/login'>login</Link>
+        </span>
+            </div>
+        </div>
+    )
+}
+
+export default Register
