c175ce864b4cc446207a7b7a9d361cda4278e5ed
Dries Dubois <dries.dubois@gmail.com>
2022-11-02 12:23:32 +0100

    + loading all data related to firebase config file

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,9 +1,24 @@
 import {initializeApp} from "firebase/app";
 import {getFirestore} from 'firebase/firestore';
+import {getAuth} from 'firebase/auth'
+
 const firebaseConfig = {
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
     projectId: process.env.REACT_APP_FIREBASE_ID,
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGESENDERID,
+    appId: process.env.REACT_APP_APPID,
+    measurementId: process.env.REACT_APP_MEASUREMENTID
 };
+
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 // Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
-export const firestoreDB = getFirestore(firebaseApp);
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+export const firestoreDB = getFirestore(app);
+export {auth}
 console.log("initialized firebase connection");
